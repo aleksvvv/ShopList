67ddffa691fee55500247f984f49327e08e4e9e9
aleksvvv <adm-shop-001@yandex.ru>
2023-03-13 11:06:29 +0300

    Domain layer

--- app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bignerdranch.android.shoplist
+package com.bignerdranch.android.shoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
