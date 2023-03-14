881825abba006bbda8ba456adca271ea794c681f
aleksvvv <adm-shop-001@yandex.ru>
2023-03-14 11:19:45 +0300

    Recyclerview. Item_shop.

--- app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
@@ -10,7 +10,7 @@ import com.bignerdranch.android.shoplist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
-    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,12 +21,6 @@ class MainActivity : AppCompatActivity() {
         viewModel.shopList.observe(this
         ) {
             Log.d("Trest", it.toString())
-            if (count === 0){
-                count++
-                val testItem = it[0]
-                viewModel.deleteShopItem(testItem)
-                viewModel.editShopItem(it[4])
-            }
 
         }
 
